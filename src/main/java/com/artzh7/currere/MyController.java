be750c0cd2483e4910ed4e4076b4042f22d25763
artzh <art.zh@inbox.ru>
2022-01-29 14:04:28 +0300

    MVC getting started

---
+++ src/main/java/com/artzh7/currere/MyController.java
@@ -0,0 +1,23 @@
+package com.artzh7.currere;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
+
+@Controller
+public class MyController {
+    @GetMapping()
+    public String main() {
+        return "main";
+    }
+
+    @GetMapping("/greeting")
+    public String greeting(
+            @RequestParam(name = "name", required = false, defaultValue = "World") String name,
+            Map<String, Object> model) {
+        model.put("name", name);
+        return "greeting";
+    }
+}
