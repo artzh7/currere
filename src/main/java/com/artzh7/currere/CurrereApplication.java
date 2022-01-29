48cb2a04da06ac0138437cc1e46dc967dfcebf22
artzh <art.zh@inbox.ru>
2022-01-29 13:56:18 +0300

    Initial commit

---
+++ src/main/java/com/artzh7/currere/CurrereApplication.java
@@ -0,0 +1,11 @@
+package com.artzh7.currere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrereApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CurrereApplication.class, args);
+    }
+}
---
+++ src/test/java/com/artzh7/currere/CurrereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artzh7.currere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrereApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
