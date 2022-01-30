893293695daa787fb06cae4bce8dff1e8886977b
artzh <art.zh@inbox.ru>
2022-01-30 11:17:45 +0300

    filters for order list

--- src/main/java/com/artzh7/currere/controller/MainController.java
+++ src/main/java/com/artzh7/currere/controller/MainController.java
@@ -1,6 +1,7 @@
 package com.artzh7.currere.controller;
 
 import com.artzh7.currere.entity.Order;
+import com.artzh7.currere.entity.OrderStatus;
 import com.artzh7.currere.repo.OrderRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,6 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -16,9 +19,21 @@ public class MainController {
     private OrderRepo orderRepo;
 
     @GetMapping("/main")
-    public String main(Map<String, Object> model) {
-        Iterable<Order> bookings = orderRepo.findAll();
-        model.put("bookings", bookings);
+    public String main(@RequestParam(required = false, defaultValue = "ALL") String orderStatus,
+                       Map<String, Object> model) {
+        List<Order> orders = new ArrayList<>();
+        try {
+            if (orderStatus.equals("WORKING")) {
+                orders.addAll(orderRepo.findAllByOrderStatus(OrderStatus.IN_WORK));
+                orders.addAll(orderRepo.findAllByOrderStatus(OrderStatus.COURIER_ARRIVED));
+                orders.addAll(orderRepo.findAllByOrderStatus(OrderStatus.IN_TRANSIT));
+            } else {
+                orders.addAll(orderRepo.findAllByOrderStatus(OrderStatus.valueOf(orderStatus)));
+            }
+        } catch (IllegalArgumentException e) {
+            orders.addAll(orderRepo.findAll());
+        }
+        model.put("orders", orders);
         return "main";
     }
 
--- src/main/java/com/artzh7/currere/repo/OrderRepo.java
+++ src/main/java/com/artzh7/currere/repo/OrderRepo.java
@@ -1,9 +1,14 @@
 package com.artzh7.currere.repo;
 
 import com.artzh7.currere.entity.Order;
+import com.artzh7.currere.entity.OrderStatus;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface OrderRepo extends CrudRepository<Order, Long> {
+    List<Order> findAll();
+    List<Order> findAllByOrderStatus(OrderStatus orderStatus);
 }
